d8fcfbe59d60c8ed7ba6d2f337cf13bb94f93ddb
ComplYue <compl.yue@icloud.com>
2020-09-24 17:16:50 +0800

    upd

--- host.hs/pub/HaskIt.hs
+++ host.hs/pub/HaskIt.hs
@@ -12,9 +12,9 @@ import           Language.Edh.EHI
 installHaskItBatteries :: EdhWorld -> IO ()
 installHaskItBatteries !world = do
 
-  void $ installEdhModule world "haskit/RT" $ \ets exit -> do
+  void $ installEdhModule world "haskit/RT" $ \ !ets !exit -> do
 
-    let moduScope = contextScope $ edh'context ets
+    let !moduScope = contextScope $ edh'context ets
 
     !moduArts <- sequence
       [ (nm, ) <$> mkHostProc moduScope mc nm hp | (mc, nm, hp) <- [] ]
@@ -27,14 +27,14 @@ installHaskItBatteries !world = do
 
     exit
 
-  void $ installEdhModule world "haskit/web/RT" $ \ets exit -> do
+  void $ installEdhModule world "haskit/web/RT" $ \ !ets !exit -> do
 
-    let moduScope = contextScope $ edh'context ets
+    let !moduScope = contextScope $ edh'context ets
 
     !moduArts <- sequence
       [ (nm, ) <$> mkHostProc moduScope mc nm hp | (mc, nm, hp) <- [] ]
 
-    artsDict <- EdhDict
+    !artsDict <- EdhDict
       <$> createEdhDict [ (EdhString k, v) | (k, v) <- moduArts ]
     flip iopdUpdate (edh'scope'entity moduScope)
       $  [ (AttrByName k, v) | (k, v) <- moduArts ]
