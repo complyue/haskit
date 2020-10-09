c18be7ff6b4e4a491bf3dcc2a825e27bb0b8cd32
ComplYue <compl.yue@icloud.com>
2020-10-09 21:50:59 +0800

    wip

--- edh_modules/haskit/web/haskit.js
+++ edh_modules/haskit/web/haskit.js
@@ -1,81 +1,96 @@
-import { uiLog, clearLog } from "/log.js";
+/**
+ * main module of HaskIt root page
+ */
 
-import WSC from "/wsc.js";
+import { Lander, WsPeer } from "/nedh/index.js";
+
+import { hasLogBox, uiLog, clearLog } from "/log.js";
 
 $("button[name=clear-log]").on("click", () => {
   clearLog();
 });
 
-const plotData = {};
+async function getWsUrl() {
+  let wsPort = await $.get("/:");
+  // todo use wss:// when appropriate
+  return "ws://" + location.hostname + ":" + wsPort;
+}
 
-// reply plot data to a haze plot window, after it reported-in
-window.onmessage = function (me) {
-  let hazeWin = me.source;
+class HaskItLander extends Lander {
+  async landingThread() {
+    // XXX a subclass of Lander normally overrides this method, with even
+    //     verbatim copy of the method code here, it's already very useful,
+    //     in that the subclass' lexical context becomes the landing
+    //     environment.
+    //
+    //     And as this function's scope is the local scope within which the
+    //     source of incoming packets are eval'ed, other forms of (maybe ugly
+    //     and nasty) hacks can be put right here, but god forbid it.
 
-  if ("haze-win-report-in" !== me.data.type) {
-    console.error("unexpected window message.");
-    debugger;
-    return;
-  }
+    if (null === this.nxt) {
+      throw Error("Passed end-of-stream for packets");
+    }
+    while (true) {
+      const [_outlet, _resolve, _reject, _intake, _inject] = this.nxt;
+      const [_src, _nxt] = await _intake;
+      this.nxt = _nxt;
+      if (null === _src) {
+        if (null !== _nxt) {
+          throw Error("bug: inconsistent eos signal");
+        }
+        return; // reached end-of-stream, terminate this thread
+      }
+      if (null === _nxt) {
+        throw Error("bug: null nxt for non-eos-packet");
+      }
 
-  let pd = plotData[hazeWin.name];
-  if (pd === undefined) {
-    // manually refresh a Haze plot window can lead into here
-    uiLog("Don't do that!", "err-msg", "Did you refresh a plot window?");
-    return; // we don't have the data anymore, no more to do
-  }
-
-  // unreference from main window for sure, to avoid leakage
-  delete plotData[hazeWin.name];
-
-  // reply the data
-  hazeWin.postMessage(
-    Object.assign(
-      {
-        type: "haze-plot-data",
-      },
-      pd
-    ),
-    "*",
-    pd.bins // transfer the typed-arrays for zero-copy performance
-  );
-};
-
-export class HaskItWSC extends WSC {
-  async plotWin(pgid, pwid, tmpl, cnl, plotCode) {
-    // take the binary chunks, temporarily store the data,
-    // open a plot window which will come retrive the data.
-    let bins = Array.from(this.bins);
-    this.bins.length = 0;
-
-    let winName = "plot@" + pgid + "%" + pwid;
-    plotData[winName] = {
-      bins,
-      pgid,
-      pwid,
-      cnl,
-      plotCode,
-    };
-    window.open(tmpl, winName);
+      // land one packet
+      try {
+        _resolve(eval(_src));
+      } catch (exc) {
+        _reject(exc);
+      }
+    }
   }
 }
 
-export const hskiWSC = new HaskItWSC();
+class HaskItPeer extends WsPeer {
+  async handleError(err, errDetails) {
+    console.error("Unexpected WS error: ", err, errDetails);
+    debugger;
+    if (hasLogBox()) uiLog(err, "err-msg", errDetails);
+  }
+  cleanup() {
+    super.cleanup();
+    uiLog("Lost connection with HaskIt backend.");
+  }
+}
 
-export async function withHaskIt(hskiAct) {
-  let ws = await hskiWSC.dial();
-  hskiAct(ws);
+let _livePeer = null;
+
+export default async function livePeer() {
+  switch (_livePeer ? _livePeer.ws.readyState : WebSocket.CLOSED) {
+    case WebSocket.OPEN:
+      return _livePeer; // already established
+    case WebSocket.CONNECTING:
+      break; // connecting inprogress
+    default:
+      // to establish new WebSocket connection with Nedh semantics
+      _livePeer = new HaskItPeer(await getWsUrl(), new HaskItLander());
+  }
+  // wait until really connected, as well as get connection error if any
+  await _livePeer.opened;
+  // certainly it's connected for the time being
+  return _livePeer;
 }
 
 $(async function () {
   try {
     uiLog("Dialing HaskIt backend ...");
-    await hskiWSC.dial();
+    await livePeer();
     uiLog("Connected with HaskIt backend.");
   } catch (err) {
     let details = err ? err.stack : err;
     uiLog("Failed connecting to HaskIt backend via ws.", "err-msg", details);
   }
 });
-
-export default hskiWSC;
