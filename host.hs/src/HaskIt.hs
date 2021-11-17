09da94ac3b54734ee31c16b5a22c0e64d469fa23
ComplYue <compl.yue@icloud.com>
2021-11-17 20:53:02 +0800

    api refactored

--- host.hs/src/HaskIt.hs
+++ host.hs/src/HaskIt.hs
@@ -7,13 +7,6 @@ import Language.Edh.EHI
 import Prelude
 
 installHaskItBatteries :: EdhWorld -> IO ()
-installHaskItBatteries !world = do
-  void $
-    installModuleM world "haskit/RT" $ do
-      !moduScope <- contextScope . edh'context <$> edhThreadState
-
-      let !moduArts = []
-
-      iopdUpdateEdh moduArts $ edh'scope'entity moduScope
-      !esExps <- prepareExpStoreM (edh'scope'this moduScope)
-      iopdUpdateEdh moduArts esExps
+installHaskItBatteries !world = runProgramM_ world $ do
+  installModuleM_ "haskit/RT" $ do
+    return ()
